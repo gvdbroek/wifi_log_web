94df81bfc8d2bc476a510b7b83dc65ef641b2b52
gvdbroek <141612818+gvdbroek@users.noreply.github.com>
2025-02-08 18:27:55 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile('dist/index.html'); // Adjust the path based on your build directory
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
+});
+
